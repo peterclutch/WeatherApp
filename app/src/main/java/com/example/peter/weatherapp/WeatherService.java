74272719e43e2f326e582f509a3ba6d7b4863ebe
Peter <peter91@cs.au.dk>
2017-05-02 12:30:45 +0200

    Created service and manifest update

---
+++ app/src/main/java/com/example/peter/weatherapp/WeatherService.java
@@ -0,0 +1,33 @@
+package com.example.peter.weatherapp;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by Peter on 02-May-17.
+ */
+
+public class WeatherService extends Service {
+    private static final String LOG = "SERVICE LOG";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(LOG, "onCreate");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+    @Nullable
+    @Override
+    //this service is not for binding and returns null
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
